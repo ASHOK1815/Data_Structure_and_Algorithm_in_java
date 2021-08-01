86635b67ed38493189ad7eabd837a5ce961734a7
ASH1815 <kumawatashok1815@gmail.com>
2021-08-01 11:25:59 +0530

    linkedlist_with_all_task:smile:

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -1,5 +1,3 @@
-
-
 class LinkedList<E>{
 
     class ListNode<E>{
@@ -53,6 +51,80 @@ class LinkedList<E>{
         tail.next  = newElement;
     }
 
+    public void insertPosition(int position,E data)
+    {
+        newElement = head;
+
+        for(int count=1;count<position;count++)
+        {
+            count++;
+            newElement = newElement.next;
+        }
+        ListNode<E> newDummyVariable = new ListNode<>(data);
+        newDummyVariable.next = newElement.next;
+        newElement.next = newDummyVariable;
+
+
+    }
+
+    public void deleteFirstNode()
+    {
+        newElement = head;
+        head =  head.next;
+    }
+
+    public void deleteLastNode()
+    {
+        newElement = head;
+        ListNode<E> dummyVariable = null;
+        while (newElement.next != null)
+        {
+            dummyVariable = newElement;
+            newElement = newElement.next;
+        }
+
+        dummyVariable.next = null;
+    }
+
+
+    public void deletePostion(int position)
+    {
+        newElement = head;
+
+        if(position == 0)
+        {
+            head = head.next;
+            return;
+        }
+
+        for(int count=1;count<position;count++)
+        {
+            newElement = newElement.next;
+        }
+
+        newElement.next = newElement.next.next;
+
+    }
+
+
+    public void displayReverse()
+    {
+        ListNode<E> currentElement = head;
+        ListNode<E> prev = null;
+        ListNode<E> next = null;
+
+        while(currentElement != null)
+        {
+            next = currentElement.next;
+            currentElement.next = prev;
+            prev = currentElement;
+            currentElement = next;
+        }
+
+        head = prev;
+        dispalyUsingRecursion(head);
+    }
+
     public void display()
     {
         ListNode<E> temp = head;
@@ -65,15 +137,28 @@ class LinkedList<E>{
 
     }
 
+    public void  dispalyUsingRecursion(ListNode head)
+    {
+        if(head == null)
+        {
+            return;
+        }
+
+        System.out.println(head.data+" ");
+        dispalyUsingRecursion(head.next);
+
+    }
+    public void displayInReverseUsingRecursion(ListNode head)
+    {
+        if(head == null)
+        {
+            return;
+        }
+        dispalyUsingRecursion(head.next);
+        System.out.println(head.data+" ");
+    }
+
 }
-
-
-
-
-
-
-
-
 
 public class LinkedListMain {
     public static void main(String[] args) {
@@ -84,6 +169,14 @@ public class LinkedListMain {
         linkedList.insert(3);
         linkedList.insertHead(100);
         linkedList.insertTail(100);
-        linkedList.display();
+        linkedList.insertPosition(2,786);
+        linkedList.deleteFirstNode();
+        linkedList.deleteLastNode();
+        linkedList.deletePostion(1);
+        linkedList.displayReverse();
+
+        //linkedList.displayInReverseUsingRecursion(linkedList.head);
+
+        //linkedList.display();
     }
 }
