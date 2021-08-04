aa9886d6e5b7e4e6da2de4078ef46a67f21b3f92
ASH1815 <kumawatashok1815@gmail.com>
2021-08-04 08:40:18 +0530

    BinearySearch:robot:

--- src/SearchingAlgorithm/BinearySearch.java
+++ src/SearchingAlgorithm/BinearySearch.java
@@ -34,26 +34,4 @@ public class BinearySearch {
         return  -1;
     }
 
-    public static void main(String[] args) {
-
-
-
-        System.out.println("Enter the size of the array");
-        int n = scanner.nextInt();
-
-        int arr[] = new int[n];
-
-        System.out.println("Enter " + n +" element ");
-
-        for(int i=0 ;i<n;i++)
-        {
-            arr[i] = scanner.nextInt();
-        }
-
-
-
-
-
-    }
-
 }
